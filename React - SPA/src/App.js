27995e3dd8d33ff9db16aa693c498b1ac99c8f76
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-11 23:19:06 -0700

    Added second route - Products

--- React - SPA/src/App.js
+++ React - SPA/src/App.js
@@ -4,12 +4,14 @@ import {
   routerProvider
 } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import Products from "./components/Products";
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <HomePage /> },
+    { path: "/products", element: <Products /> },
   ]);
-  
+
   return <RouterProvider router={router} />;
 }
 
