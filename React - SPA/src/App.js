e6a2f66fd55231af96b88cc5db3e939ca8615223
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-11 23:25:10 -0700

    Explored older alternaive method

--- React - SPA/src/App.js
+++ React - SPA/src/App.js
@@ -1,17 +1,32 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  routerProvider
+  createRoutesFromElements,
+  Route
 } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Products from "./components/Products";
 
-function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: <HomePage /> },
-    { path: "/products", element: <Products /> },
-  ]);
+{
+  /*
 
+    const routeDefinitions = createRoutesFromElements(
+      <Route>
+        <Route path="/" element = {<HomePage />} />
+        <Route path="/products" element = {<Products />} />
+      </Route>
+    );
+    const router = createBrowserRouter(routeDefinitions)
+
+*/
+}
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <Products /> }
+]);
+
+function App() {
   return <RouterProvider router={router} />;
 }
 
