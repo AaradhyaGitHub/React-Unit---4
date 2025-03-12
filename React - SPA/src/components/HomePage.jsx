963da4114f501b0f62b1e25490e0a758620a8516
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-11 23:33:24 -0700

    added Link and to properties to link 2 pages

--- React - SPA/src/components/HomePage.jsx
+++ React - SPA/src/components/HomePage.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default function HomePage() {
-  return <h1>This is my home page!!</h1>;
+  return(
+  <>
+    <h1>This is my home page!!</h1>
+    <p>
+        Go to <Link to="/products">The list of products</Link>
+    </p>
+  </>)
 }
