c472d88ba568f7f424d32bcba319875a428cab85
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-12 22:25:19 -0700

    Error handling for unsundefined routes

--- React - SPA/src/App.js
+++ React - SPA/src/App.js
@@ -6,7 +6,9 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Products from "./pages/Products";
+import ErrorPage from "./pages/Error";
 import RootLayout from "./pages/Root";
+
 
 {
   /*
@@ -26,6 +28,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage /> ,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <Products /> }
