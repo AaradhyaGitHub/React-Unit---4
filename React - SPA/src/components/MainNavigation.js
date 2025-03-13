7e33b8eac66dea3b5bba2e4fdc64be077ff6cee4
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-12 23:03:17 -0700

    Conditional active Navhighlight

--- React - SPA/src/components/MainNavigation.js
+++ React - SPA/src/components/MainNavigation.js
@@ -1,15 +1,38 @@
-import { Link } from "react-router-dom";
-import classes from './MainNavigation.module.css'
+import { NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+//NavLink can be a replacement for Link
+//NavLink takes a className prop
+//  which takes
+//            --> a function parameter
+//  which takes
+//            --> an object parameter
+//  which takes
+//            --> an isActive parameter
 export default function MainNavigation() {
   return (
     <header className={classes.header}>
       <nav>
-        <ul className = {classes.list}>
+        <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
