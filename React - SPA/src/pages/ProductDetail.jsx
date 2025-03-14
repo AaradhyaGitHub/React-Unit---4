eaed653d0834d3da69e50384bc885442d8cdc6b4
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-13 22:40:22 -0700

    Using relative prop to remove URL segment

--- React - SPA/src/pages/ProductDetail.jsx
+++ React - SPA/src/pages/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 export default function ProductDetail() {
   const params = useParams();
@@ -7,6 +7,22 @@ export default function ProductDetail() {
     <>
       <p>Product Details...</p>
       <p>{params.productId}</p>
+      {/*
+            This by default is a relative path --> <p><Link to="..">Back</Link></p>
+
+            Relative to route definition. In this case, when we go up, we go up to parent which is /root 
+
+            instead we want to go to prodcuts
+
+            adding relative = "path" --> React will remove one segment...
+            in this case, the id is removed and we go back to /products
+
+        */}
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
     </>
   );
 }
