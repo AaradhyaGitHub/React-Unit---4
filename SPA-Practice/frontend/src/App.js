83e33a2388bacc4d62cfe9fdb3cbf1e792e6a5b6
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-14 23:26:42 -0700

    Moved loader fetch function to respective page file

--- SPA-Practice/frontend/src/App.js
+++ SPA-Practice/frontend/src/App.js
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import EventsPage from "../src/pages/EventsPage";
+import EventsPage, { loader as eventsLoader } from "../src/pages/EventsPage";
 import EventDetailPage from "../src/pages/EventDetailPage";
 import NewEventPage from "../src/pages/NewEventPage";
 import RootLayout from "./pages/RootLayout";
@@ -19,17 +19,7 @@ const router = createBrowserRouter([
             index: true,
             path: "",
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch("http://localhost:8080/events");
-
-              if (!response.ok) {
-              } else {
-                const resData = await response.json();
-                return resData.events;
-                // this returned data is automatically available in Events page
-                // it's available in other components too
-              }
-            }
+            loader: eventsLoader
           },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
@@ -39,30 +29,6 @@ const router = createBrowserRouter([
     ]
   }
 ]);
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 function App() {
   return (
