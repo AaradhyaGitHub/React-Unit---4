bd4bc5f4ba24c2d636061ed4f8d98a7bb826b8af
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-18 12:47:37 -0700

    UX improvement for form submission feedback

--- SPA-Practice/frontend/src/components/EventForm.js
+++ SPA-Practice/frontend/src/components/EventForm.js
@@ -1,14 +1,18 @@
-import { useNavigate, Form } from "react-router-dom";
+import { useNavigate, Form, useNavigation } from "react-router-dom";
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
   function cancelHandler() {
     navigate("..");
   }
 
   return (
-    <Form method="POST" className={classes.form} >
+    <Form method="POST" className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
         <input
@@ -50,10 +54,10 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>{isSubmitting?'Submitting': 'Save'}</button>
       </div>
     </Form>
   );
