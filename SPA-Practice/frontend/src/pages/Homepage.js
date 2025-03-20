8e03632dc7a9e64d4919d0d3f7f683ad314dac69
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-19 22:12:42 -0700

    Project Finished

--- SPA-Practice/frontend/src/pages/Homepage.js
+++ SPA-Practice/frontend/src/pages/Homepage.js
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom"
-export default function HomePage(){
-    return(
-        <>
-            <h1>This is the HomePage</h1>
-            <p><Link to=".." relative="path">
-          Back
-        </Link></p>
-        </>
-    )
+import PageContent from "../components/PageContent";
+export default function HomePage() {
+  return (
+    <>
+      <PageContent title="Welcome!">
+        <p>Browse our upcoming events!</p>
+      </PageContent>
+    </>
+  );
 }
