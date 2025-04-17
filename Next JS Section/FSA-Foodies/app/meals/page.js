396482186aeb1079d654d1e968a353308582bf47
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-16 23:29:05 -0700

    Added loading state and imported data from DB

--- Next JS Section/FSA-Foodies/app/meals/page.js
+++ Next JS Section/FSA-Foodies/app/meals/page.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 export default function MealsPage() {
+  const meals = getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -11,13 +13,11 @@ export default function MealsPage() {
         </h1>
         <p>Choose your fav recipe amd cook it yourself. Easy and fun!</p>
         <p className={classes.cta}>
-          <Link href="/meals/share">
-            Share your fav recipe
-          </Link>
+          <Link href="/meals/share">Share your fav recipe</Link>
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
