dc9abb91315893099e61890aec4e4ddca4d38c3b
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-17 13:50:03 -0700

    Added get meal functionality to display meal item information

--- Next JS Section/FSA-Foodies/lib/meals.js
+++ Next JS Section/FSA-Foodies/lib/meals.js
@@ -1,9 +1,13 @@
-import sql from 'better-sqlite3';
+import sql from "better-sqlite3";
 
-const db = sql('meals.db');
+const db = sql("meals.db");
 
-export async function getMeals(){
-   await new Promise((resolve) => setTimeout(resolve,5000))
-   // throw new Error('Loading meals failed');
-   return db.prepare('SELECT * FROM meals').all();
+export async function getMeals() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // throw new Error('Loading meals failed');
+  return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+  return db.prepare("select * from MEALS WHERE slug = ?").get(slug);
 }
