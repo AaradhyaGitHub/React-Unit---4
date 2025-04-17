d66f59b6f59721c8f0b1cb1b76229c5945cd39e1
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-16 19:44:17 -0700

    Created component for meals item and imported css for meals page

--- Next JS Section/FSA-Foodies/app/meals/page.js
+++ Next JS Section/FSA-Foodies/app/meals/page.js
@@ -1,9 +1,23 @@
 import Link from "next/link";
+import classes from "./page.module.css";
 export default function MealsPage() {
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your fav recipe amd cook it yourself. Easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share your fav recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        
+      </main>
     </>
   );
 }
