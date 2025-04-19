145385ed148da4d6d27ed182ce554c0548313171
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-19 15:33:08 -0700

    Initialized page router app

---
+++ Next JS Section/pages-router-newsapp/pages/index.js
@@ -0,0 +1,9 @@
+// main root file -> domain.com/
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>This is the home page</h1>
+    </>
+  );
+}
---
+++ Next JS Section/pages-router-newsapp/pages/news.js
@@ -0,0 +1,9 @@
+// news file -> domain.com/news
+
+export default function NewsPage() {
+  return (
+    <>
+      <h1>This is the news page</h1>
+    </>
+  );
+}
