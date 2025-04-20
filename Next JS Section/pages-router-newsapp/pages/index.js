31d5dcefd699b97efc041480d4d5ecf81b79d2a1
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-19 18:56:08 -0700

    Basic Link navigation

--- Next JS Section/pages-router-newsapp/pages/index.js
+++ Next JS Section/pages-router-newsapp/pages/index.js
@@ -1,9 +1,14 @@
 // main root file -> domain.com/
-
+import Link from "next/link";
 export default function HomePage() {
   return (
     <>
       <h1>This is the home page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js-rocks">Next.js Rocks</Link>
+        </li>
+      </ul>
     </>
   );
 }
--- Next JS Section/pages-router-newsapp/pages/news/index.js
+++ Next JS Section/pages-router-newsapp/pages/news/index.js
@@ -4,6 +4,10 @@ export default function NewsPage() {
   return (
     <>
       <h1>This is the news page</h1>
+      <ul>
+        <li>Next.js Rocks</li>
+        <li>Page Router doesn't rock as much</li>
+      </ul>
     </>
   );
 }
