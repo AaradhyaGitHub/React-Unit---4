d16b12a8596c3c7de02aca32b11b0a58dac33c56
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-19 23:54:53 -0700

    Popualting page with dummy content

--- Next JS Section/pages-router-newsapp/pages/index.js
+++ Next JS Section/pages-router-newsapp/pages/index.js
@@ -1,7 +1,26 @@
-export default function HomePage(){
-    return(
-        <>
-            <h1>This is the home page</h1>
-        </>
-    )
+import MeetupList from "../components/meetups/MeetupList";
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First Meetup",
+    image:
+      "https://theinsatiabletraveler.com/wp-content/uploads/2016/02/Cuba-Photography-Havana-584902.jpg",
+    address: "123 Romero Yoel St, Havana, Cuba",
+    description: "This is the first meetup"
+  },
+  {
+    id: "m2",
+    title: "Second Meetup",
+    image:
+      "https://plus.unsplash.com/premium_photo-1716932567535-6bb42a3f38ff?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dmllbm5lfGVufDB8fDB8fHww",
+    address: "456 Billy Joel St, Vienne, France",
+    description: "This is the second meetup"
+  }
+];
+export default function HomePage() {
+  return (
+    <>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </>
+  );
 }
