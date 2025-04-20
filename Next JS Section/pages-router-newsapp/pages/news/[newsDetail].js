3f7bc989d310d587796ca863d2317fbbcfb17fbf
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-19 17:49:00 -0700

    extracting url info using next react hook

--- Next JS Section/pages-router-newsapp/pages/news/[newsDetail].js
+++ Next JS Section/pages-router-newsapp/pages/news/[newsDetail].js
@@ -1,6 +1,10 @@
 // news file -> domain.com/news/news-detail
-
+import {useRouter} from 'next/router'
 export default function NewsDetail() {
+  const router = useRouter();
+  const newsDetail = router.query.newsDetail
+  //send request to backend API -> fetch newsItem with newsId
+  
   return (
     <>
       <h1>This is the news details page</h1>
