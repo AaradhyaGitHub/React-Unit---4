1fb9677194e204e22f34617217867eb4d7cba6c0
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-19 20:27:19 -0700

    Added and populated meetup project

--- Next JS Section/pages-router-newsapp/pages/index.js
+++ Next JS Section/pages-router-newsapp/pages/index.js
@@ -1,14 +1,7 @@
-// main root file -> domain.com/
-import Link from "next/link";
-export default function HomePage() {
-  return (
-    <>
-      <h1>This is the home page</h1>
-      <ul>
-        <li>
-          <Link href="/news/next-js-rocks">Next.js Rocks</Link>
-        </li>
-      </ul>
-    </>
-  );
+export default function HomePage(){
+    return(
+        <>
+            <h1>This is the home page</h1>
+        </>
+    )
 }
