7d5cb0d9307bb879a543025241684394e4cfb375
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-21 23:11:35 -0700

    Added basic backend API

---
+++ Next JS Section/pages-router-newsapp/pages/api/new-meetup.js
@@ -0,0 +1,12 @@
+// api/new-meetup
+
+//
+function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.data;
+    const { title, image, address, description } = data;
+
+  }
+}
+
+export default handler;
