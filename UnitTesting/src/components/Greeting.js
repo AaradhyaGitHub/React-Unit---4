ad59139e76977edb9096792a0363faca65cdcf45
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-28 13:31:37 -0700

    Added first unit test

--- UnitTesting/src/components/Greeting.js
+++ UnitTesting/src/components/Greeting.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
 export default function Greeting() {
+  const [changeText, setChangeText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangeText(true);
+  };
   return (
     <div>
       <h2>Hello World</h2>
-      <p>Good to see ya mate</p>
+
+      {!changeText ? <p>Good to see ya mate</p> : <p>Howdy señor!</p>}
+      <button onClick={changeTextHandler}>Change Txt</button>
     </div>
   );
 }
+
