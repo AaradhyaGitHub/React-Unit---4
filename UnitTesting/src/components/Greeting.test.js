3989d08b5793d3ad6d98df377fb6a34fa36a4fdd
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-28 12:54:19 -0700

    Added first test for exact wording match

---
+++ UnitTesting/src/components/Greeting.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+test("Renders hello world as a text", () => {
+  //Arrannge:
+  render(<Greeting />);
+
+  //Act:
+  //....nothing here
+
+  //Assert: Test whether hello world is rendered as a text:
+  const helloWorldElement = screen.getByText("Hello World", { exact: true });
+  expect(helloWorldElement).toBeInTheDocument();
+
+  // get functions -> throws error, query functions don't do that
+  // find functions -> returns a promise
+});
+
+// The three A's of testing:
+
+// Arrange: Set up the test data, test conditions and test environment
+// Act: Run logic that should be tested (ex: execute function)
+// Assert: Compare execution results with expected results
