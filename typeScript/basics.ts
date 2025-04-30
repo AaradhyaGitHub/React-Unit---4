3b5f9691867ffff1c0f321661d280fb8e694431b
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-29 19:57:44 -0700

    Explored Union Types

--- typeScript/basics.ts
+++ typeScript/basics.ts
@@ -13,4 +13,36 @@ let isInstructor: boolean;
 isInstructor = false;
 
 
+//arrays 
+let hobbies: string[]; //array of strings 
+let importantDates: number[]; //array of numbers
 
+//object 
+//This object works fine
+let person; 
+person = {
+    name: 'Max',
+    age: 32
+}
+
+//object type
+let typescriptPerson: {
+    name: string, 
+    age: number
+}[];
+
+typescriptPerson = [{
+    age: 19,
+    name: "John Cena"
+}]
+
+//type inference: 
+let title = 'BJJ - The complete sport';
+// title = 1;  We get this error because of type inference. It tries to infer which type goes where. 
+// what if we have more than 2 data types that need to belong to one variable:
+
+
+//union type
+let course: string | number = 'Armbar - Game of isolation'
+course = 1234;
+//this works
