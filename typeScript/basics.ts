5fa4e52e1d23dad41e2dbed4ecdbdcee3ffe7e0f
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-04-29 18:13:49 -0700

    Exploring primitive data types in TS

---
+++ typeScript/basics.ts
@@ -0,0 +1,16 @@
+// Prims: Number, String, Boolean 
+// Complex type: Arrays and Objects 
+// Function types like params 
+
+//prims 
+let age: number;
+age = 22;
+
+let userName : string;
+userName = "Donny";
+
+let isInstructor: boolean;
+isInstructor = false;
+
+
+
