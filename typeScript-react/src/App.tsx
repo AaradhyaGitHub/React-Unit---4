c8306246ad9b3c489621e39db91d33eff2c7f19a
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-05-01 22:04:02 -0700

    Created data model

--- typeScript-react/src/App.tsx
+++ typeScript-react/src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
+import Todo from './models/todo';
 import Todos from './components/Todos';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Typescript'),
+    new Todo('Make Project')
+  ]
   return (
     <div>
-      <Todos items={[
-        'Learn React',
-        'Learn Typescript',
-        'Make Project'
-      ]}/>
+      <Todos items={todos}/>
     </div>
   );
 }
--- typeScript-react/src/components/Todos.tsx
+++ typeScript-react/src/components/Todos.tsx
@@ -1,10 +1,11 @@
-const Todos: React.FC<{items: string[]}> = (props) => {
-    return (
-        <ul>
-            {
-                props.items.map(item => <li key={item}>{item}</li>)
-            }
-        </ul>
-    )
-}
+import Todo from "../models/todo";
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
+};
 export default Todos;
