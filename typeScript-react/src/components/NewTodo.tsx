0078dc3d604864a2a112fc2c23c919c4aa3c37c0
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-05-03 20:37:25 -0700

    Implemented Context with TypeScript

--- typeScript-react/src/components/NewTodo.tsx
+++ typeScript-react/src/components/NewTodo.tsx
@@ -1,8 +1,8 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
+import { TodosContext } from "../store/todos-context";
 import classes from "./NewTodo.module.css";
-const NewTodo: React.FC<{ onAddTodo: (enteredText: string) => void }> = (
-  props
-) => {
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -13,7 +13,7 @@ const NewTodo: React.FC<{ onAddTodo: (enteredText: string) => void }> = (
       return;
     }
 
-    props.onAddTodo(enteredText);
+    todosCtx.addTodo(enteredText);
   };
 
   return (
